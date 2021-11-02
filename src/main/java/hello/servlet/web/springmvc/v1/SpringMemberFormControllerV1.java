9bc0ec667b9d09eaef19ba876baf75e54aec5b95
SeokHyunKho <seokhyunkho@gmail.com>
2021-11-02 23:25:29 +0900

    [스프링 MVC - 구조 이해] 스프링 MVC - 시작하기

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+// 이 둘을추가하면 @Controller 처럼 찾아가는것이 가능하다.
+//@Component
+//@RequestMapping
+@Controller
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
